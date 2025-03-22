c6e6496d8c9f2e68cafc429ca58a89368ed8682a
Hespian <kamil.gurlaga1999@gmail.com>
2025-03-22 19:26:14 +0100

    test2

--- geocomp.r
+++ geocomp.r
@@ -1,1 +1,4 @@
-print("Hello")
+install.packages("remotes")
+install.packages("geocompkg",
+repos = c("https://geocompr.r-universe.dev", "https://cloud.r-project.org"), # nolint
+    dependencies = TRUE, force = TRUE) # nolint
