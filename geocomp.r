a0b013177496d5604ad5bec4f9553f064235e872
Hespian <kamil.gurlaga1999@gmail.com>
2025-03-22 19:42:52 +0100

    map basic

--- geocomp.r
+++ geocomp.r
@@ -1,4 +1,16 @@
 install.packages("remotes")
 install.packages("geocompkg",
-repos = c("https://geocompr.r-universe.dev", "https://cloud.r-project.org"), # nolint
-    dependencies = TRUE, force = TRUE) # nolint
+    repos = c("https://geocompr.r-universe.dev", "https://cloud.r-project.org"),
+    dependencies = TRUE, force = TRUE
+)
+
+# Generate map
+library(leaflet)
+popup = c("Robin", "Jakub", "Jannes")
+leaflet() |>
+    addProviderTiles("NASAGIBS.ViirsEarthAtNight2012") |>
+    addMarkers(
+        lng = c(-3, 23, 11),
+        lat = c(52, 53, 49),
+        popup = popup
+    )
