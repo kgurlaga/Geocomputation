92f38a5f52f6862942f82c6ac24d7e9a0e08942d
Hespian <kamil.gurlaga1999@gmail.com>
2025-03-23 20:28:20 +0100

    simple features

--- geocomp.r
+++ geocomp.r
@@ -14,3 +14,19 @@ leaflet() |>
         lat = c(52, 53, 49),
         popup = popup
     )
+
+library(sf)
+library(terra)
+library(spData)
+library(spDataLarge)
+
+vignette(package = "sf")
+vignette("sf1")
+
+class(world)
+names(world)
+plot(world)
+
+summary(world["lifeExp"])
+
+world_mini = world[1:2, 1:3]
