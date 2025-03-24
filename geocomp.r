76c7d9a60a00f344c652656ae9f225e0eaf31ede
Hespian <kamil.gurlaga1999@gmail.com>
2025-03-24 21:01:12 +0100

    simple maps

--- geocomp.r
+++ geocomp.r
@@ -30,3 +30,27 @@ plot(world)
 summary(world["lifeExp"])
 
 world_mini = world[1:2, 1:3]
+
+world_dfr = st_read(system.file("shapes/world.gpkg", package="spData"))
+world_tbl = read_sf(system.file("shapes/world.gpkg", package="spData"))
+class(world_dfr)
+class(world_tbl)
+
+# Basic maps
+plot(world[3:6])
+plot(world["pop"])
+
+world_asia = world[world$continent == "Asia",]
+asia = st_union(world_asia)
+
+plot(world["pop"], reset=FALSE)
+plot(asia, add=TRUE, col ="#ff0000")
+
+plot(world["continent"], reset = FALSE)
+cex = sqrt(world$pop) / 10000
+world_cents = st_centroid(world, of_largest = TRUE)
+plot(st_geometry(world_cents), add = TRUE, cex = cex)
+
+india = world[world$name_long == "India", ]
+plot(st_geometry(india), expandBB = c(0, 0.2, 0.1, 1), col = "gray", lwd = 3)
+plot(st_geometry(world_asia), add = TRUE)
