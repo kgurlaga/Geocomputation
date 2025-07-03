bfccddfa8e8ff854f92221fb496c2e5d77415984
Hespian <kamil.gurlaga1999@gmail.com>
2025-07-03 20:41:44 +0200

    geographic I/O

--- geocomp.r
+++ geocomp.r
@@ -992,3 +992,92 @@ snow = rast(myurl)
 
 rey = data.frame(lon = -21.94, lat = 64.15)
 snow_rey = extract(snow, rey)
+
+write_sf(obj = world, dsn = "world.gpkg")
+
+write_sf(obj = world, dsn = "world_many_layers.gpkg", layer = "second_layer")
+
+st_write(obj = world, dsn = "world2.gpkg")
+
+write_sf(cycle_hire_xy, "cycle_hire_xy.csv", layer_options = "GEOMETRY=AS_XY")
+write_sf(world_wkt, "world_wkt.csv", layer_options = "GEOMETRY=AS_WKT")
+
+writeRaster(single_layer, filename = "my_raster.tif", datatype = "INT2U")
+
+writeRaster(x = single_layer, filename = "my_raster.tif", gdal = c("COMPRESS=NONE"), overwrite=TRUE)
+writeRaster(x = single_layer, filename = "my_raster.tif", filetype = "COG", overwrite = TRUE)
+
+download.file(url = "https://hs.pangaea.de/Maps/PeRL/PeRL_permafrost_landscapes.zip", destfile = "PeRL_permafrost_landscapes.zip", mode="wb")
+unzip("PeRL_permafrost_landscapes.zip")
+canada_perma_land <- read_sf("PeRL_permafrost_landscapes/canada_perma_land.shp")
+
+library(rnaturalearth)
+usa_sf = ne_countries(country = "United States of America", returnclass = "sf")
+plot(usa_sf[1])
+
+library(geodata)
+worldclim_prec = worldclim_global("prec", res = 10, path = tempdir())
+class(worldclim_prec)
+
+library(osmdata)
+parks = opq(bbox = "leeds uk") %>%
+    add_osm_feature(key = "leisure", value = "park") %>%
+    osmdata_sf()
+
+world2 = spData::world
+world3 = read_sf(system.file("shapes/world.gpkg", package = "spData"))
+
+library(tidygeocoder)
+geo_df = data.frame(address = "54 Frith St, London W1D 4SJ, UK")
+geo_df = geocode(geo_df, address, method = "osm")
+geo_df
+geo_sf = st_as_sf(geo_df, coords = c("long", "lat"), crs = "EPSG:4326")
+plot(geo_sf)
+
+library(geometa)
+md <- ISOMetadata$new()
+md$validate()
+xml = md$encode
+md$save("my_metadata.xml")
+md = readISO19139("my_metadata.xml")
+
+library(httr)
+base_url = "https://www.fao.org"
+endpoint = "/fishery/geoserver/wfs"
+q = list(request = "GetCapabilities")
+res = GET(url = modify_url(base_url, path = endpoint), query = q)
+res$url
+
+txt = content(res, "text")
+xml = xml2::read_xml(txt)
+xml
+
+library(sf)
+sf::sf_use_s2(FALSE)
+qf = list(request = "GetFeature", typeName = "fifao:FAO_MAJOR")
+file = tempfile(fileext = ".gml")
+GET(url = base_url, path = endpoint, query = qf, write_disk(file))
+fao_areas = read_sf(file)
+
+library(ows4R)
+
+WFS <- WFSClient$new(
+    url = "https://www.fao.org/fishery/geoserver/wfs",
+    serviceVersion = "1.0.0",
+    logger = "INFO"
+)
+
+caps = WFS$getCapabilities
+features = WFS$getFeatures("fifao:FAO_MAJOR")
+
+png(filename = "lifeExp.png", width = 500, height = 350)
+plot(world["lifeExp"])
+dev.off()
+
+library(tmap)
+tmap_obj = tm_shape(world) + tm_polygons(col = "lifeExp")
+tmap_save(tmap_obj, filename = "lifeExp_tmap.png")
+
+library(mapview)
+mapview_obj = mapview(world, zcol = "lifeExp", legend = TRUE)
+mapshot2(mapview_obj, url="my_interactive_map.html")
